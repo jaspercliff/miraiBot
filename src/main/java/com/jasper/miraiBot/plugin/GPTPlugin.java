7cce44e8dc193fc47ce4020adcb75a9bda4354d9
jasper <shamsjasper@gmail.com>
2023-11-20 21:24:04 +0800

    init

--- src/main/java/com/jasper/miraiBot/plugin/GPTPlugin.java
+++ src/main/java/com/jasper/miraiBot/plugin/GPTPlugin.java
@@ -1,17 +1,15 @@
 package com.jasper.miraiBot.plugin;
 
-import lombok.Data;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.mamoe.mirai.event.GlobalEventChannel;
 import net.mamoe.mirai.event.Listener;
-import net.mamoe.mirai.event.SimpleListenerHost;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.internal.deps.io.ktor.client.engine.ProxyType;
 import net.mamoe.mirai.message.data.At;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
 import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +17,7 @@ import org.springframework.context.annotation.Configuration;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.util.concurrent.CompletableFuture;
 
 @Configuration
 @ConfigurationProperties(prefix = "gpt")
@@ -31,26 +30,27 @@ public class GPTPlugin{
 
 
     @Bean
-    public Listener<GroupMessageEvent> gptListener(){
-        return GlobalEventChannel.INSTANCE.exceptionHandler(e->{
+    public Listener<GroupMessageEvent> gptListener() {
+        return GlobalEventChannel.INSTANCE.exceptionHandler(e -> {
             System.out.println("e = " + e);
-        }).subscribeAlways(GroupMessageEvent.class, event->{
-            System.out.println(trigger+"==================");
-            //        发送者qq
+        }).subscribeAlways(GroupMessageEvent.class, event -> {
+            // 发送者qq
             long senderQQ = event.getSender().getId();
             String message = event.getMessage().contentToString();
-            if (message.contains(trigger)){
+            if (message.contains(trigger)) {
                 String question = message.substring(message.indexOf(trigger) + 1);
                 MessageChainBuilder reply = new MessageChainBuilder();
 
-                String answer = generateResponse(question);
-
-                At atSender = new At(senderQQ);
-                reply.add(atSender);
-                reply.add(":");
-                reply.add(answer);
-                // 发送回复消息
-                event.getGroup().sendMessage(reply.build());
+                CompletableFuture.runAsync(() -> {
+                        String answer = generateResponse(question);
+                        System.out.println(answer);
+                        At atSender = new At(senderQQ);
+                        reply.add(atSender);
+                        reply.add(":");
+                        reply.add(answer);
+                        // 发送回复消息
+                        event.getGroup().sendMessage("hhhh");
+                });
             }
         });
     }
@@ -59,8 +59,10 @@ public class GPTPlugin{
         OkHttpClient client = new OkHttpClient.Builder()
                 .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 1081))).
                 build();
+
         // 构建请求体 JSON
-        String jsonBody = "{\"model\": \"gpt-3.5-turbo-instruct\", \"prompt\": \"" + prompt + "\", \"max_tokens\": 7, \"temperature\": 0}";
+        String jsonBody = "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \""+prompt+"\" }]}";
+
 
         // 构建请求体
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonBody);
@@ -76,7 +78,6 @@ public class GPTPlugin{
             Response response = client.newCall(request).execute();
             if (response.isSuccessful()) {
                 // 处理响应，这里简单地返回生成的文本
-                assert response.body() != null;
                 return response.body().string();
             } else {
                 // 处理错误情况
@@ -112,4 +113,25 @@ public class GPTPlugin{
         GPTPlugin.secret = secret;
     }
 
+    public static String parseContent(String jsonResponse) {
+        try {
+            // 使用 Jackson 进行解析
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+
+            // 提取 content 字段
+            String content = jsonNode
+                    .path("choices")
+                    .path(0)
+                    .path("message")
+                    .path("content")
+                    .asText();
+
+            return content;
+        } catch (Exception e) {
+            // 处理解析异常
+            e.printStackTrace();
+            return "Error parsing response.";
+        }
+    }
 }
